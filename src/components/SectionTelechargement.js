30776c06bd8fdb1f3460d348c0465e395bfae739
gerobatest <108824541+gerobatest@users.noreply.github.com>
2022-08-12 16:42:11 +0400

    Update SectionTelechargement.js

--- src/components/SectionTelechargement.js
+++ src/components/SectionTelechargement.js
@@ -7,7 +7,7 @@ import  GetTelechargment from './get/GetTelechargement';
 
 function SectionTelechargement() {
 
-    const url_api = "http://localhost:9000/telechargement/";
+    const url_api = "https://gero-api.herokuapp.com/telechargement/";
 
     //notes sera l'état des donées reçues depuis la base de donnée
     const [notes, getNotes] = useState('');
