14ed9c4c4f36623ab16a3dba416f47f3f18c8bf4
gerobatest <108824541+gerobatest@users.noreply.github.com>
2022-08-16 15:18:57 +0400

    Add files via upload

--- src/components/get/GetAccueil.js
+++ src/components/get/GetAccueil.js
@@ -8,11 +8,12 @@ export default function GetAccueil(props) {
 
         if(text.length > 0){
             return(
-                text.map((text, index) => {
-                    //console.log(text);
+                text.map((text) => {
+                    //console.log(text); loop?? 
+                    
                     //La partie qu'on veut montrer dans la page depuis la base de données
                     return(
-                        <div key={index}>
+                        <div key={text}>
                             <p className="paragraph">
                                 {text.para1}
                             </p>
---
+++ src/components/get/GetModeExistant.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function GetModeExistant(props) {
+
+        const displayText = (props) =>{
+        
+        const {text} = props; //text est un props
+
+        if(text.length > 0){
+            return(
+                text.map((text) => {
+                    //console.log(text); 
+                    
+                    //La partie qu'on veut montrer dans la page depuis la base de données
+                    return(
+                        <div className="sm-container" key={text}>
+                            <p className="sm-paragraph">
+                                {text.para1}   
+                            </p>
+                            <p className="sm-paragraph">
+                                {text.para2}
+                            </p> 
+                        </div>
+                    )
+                })
+            )  
+        }
+    }
+
+    return (
+        <>
+            {displayText(props)}
+        </>
+    )
+}
